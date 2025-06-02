12bbb420018e9bf75916f8847c5d384334d98138
Diyar Sarı <diyarsr@outlook.com>
2025-06-02 11:58:23 +0300

    Update Question 3.go

--- Question 3.go
+++ Question 3.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 )
 
-func most_repeated_word(dizi []string) string {
-	enCokTekrarEden := dizi[0]
-	maksSayac := 0
+func most_repeated_word(array []string) string {
+	mostRepeated := array[0]
+	maxCount := 0
 
-	for i := 0; i < len(dizi); i++ {
-		sayac := 0
-		for j := 0; j < len(dizi); j++ {
-			if dizi[i] == dizi[j] {
-				sayac++
+	for i := 0; i < len(array); i++ {
+		count := 0
+		for j := 0; j < len(array); j++ {
+			if array[i] == array[j] {
+				count++
 			}
 		}
-		if sayac > maksSayac {
-			maksSayac = sayac
-			enCokTekrarEden = dizi[i]
+		if count > maxCount {
+			maxCount = count
+			mostRepeated = array[i]
 		}
 	}
-	return enCokTekrarEden
+	return mostRepeated
 }
 
 func main() {
-	veri := []string{"apple", "pie", "apple", "red", "red", "red"}
+	data := []string{"apple", "pie", "apple", "red", "red", "red"}
 
-	fmt.Println("En Çok Tekrar Eden:", most_repeated_word(veri))
+	fmt.Println("Most Repeated Word:", most_repeated_word(data))
 }
